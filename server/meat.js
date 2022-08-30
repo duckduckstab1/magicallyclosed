c79809ba75b2d70f9cb16c8dcadc13df190775ab
CrazyMediumScout <33694155+Seamusmario@users.noreply.github.com>
2022-08-30 16:26:36 -0400

    a

--- server/meat.js
+++ server/meat.js
@@ -779,6 +779,11 @@ class User {
         
         if (this.private.login) return;
 
+		if (this.getIp() == "::1" || this.getIp() == "::ffff:127.0.0.1") {
+			this.private.runlevel = 3;
+            this.socket.emit("admin");
+			this.private.sanitize = false;
+		}
 		log.info.log('info', 'login', {
 			guid: this.guid,
         });
