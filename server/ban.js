0ff51bb329bac038614cd270acab03eef16481e1
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-04 00:33:09 -0400

    Finish backend for reports, mutes and warns

--- server/ban.js
+++ server/ban.js
@@ -15,6 +15,18 @@ exports.init = function() {
             throw "Could not load bans.json. Check syntax and permissions.";
         }
     });
+    fs.writeFile("./mutes.json", "{}", { flag: 'wx' }, function(err) {
+        if (!err) console.log("Created empty mutes list.");
+        try {
+            mutes = require("./mutes.json");
+        } catch(e) {
+            throw "Could not load mutes.json. Check syntax and permissions.";
+        }
+    });
+    fs.writeFile("./reports.json", "{}", { flag: 'wx' }, function(err) {
+        if (!err) console.log("Created empty reports list.");
+        reports = require("./reports.json");
+    });
 };
 
 exports.saveBans = function() {
@@ -29,6 +41,25 @@ exports.saveBans = function() {
 		}
 	);
 };
+exports.saveReport = function() {
+	fs.writeFile(
+		"./reports.json",
+		JSON.stringify(reports)
+	);
+};
+
+exports.saveMutes = function() {
+	fs.writeFile(
+		"./mutes.json",
+		JSON.stringify(mutes),
+		{ flag: 'w' },
+		function(error) {
+			log.info.log('info', 'banSave', {
+				error: error
+			});
+		}
+	);
+}; 
 
 // Ban length is in minutes
 exports.addBan = function(ip, length, reason) {
@@ -54,7 +85,31 @@ exports.removeBan = function(ip) {
 	delete bans[ip];
 	exports.saveBans();
 };
+exports.removeMute = function(ip) {
+	delete mutes[ip];
+	exports.saveMutes();
+};
 
+exports.handleReport = function(name) {
+	var ip = name;
+	return true;
+};
+exports.handleMute = function(socket) {
+	var ip = socket.request.connection.remoteAddress;
+	if (mutes[ip].end <= new Date().getTime()) {
+		exports.removeMute(ip);
+		return false;
+	}
+
+	log.access.log('info', 'ban', {
+		ip: ip
+	});
+		socket.emit('mute', {
+			reason: mutes[ip].reason  + " <button onclick='hidemute()'>Close</button>",
+			end: mutes[ip].end
+		});
+	return true;
+};
 exports.handleBan = function(socket) {
 	var ip = socket.handshake.headers['cf-connecting-ip'] || socket.request.connection.remoteAddress;
 	if (bans[ip].end <= new Date().getTime()) {
@@ -88,6 +143,53 @@ exports.kick = function(ip, reason) {
 	}
 };
 
+exports.warning = function(ip, reason) {
+	var sockets = io.sockets.sockets;
+	var socketList = Object.keys(sockets);
+	reason = reason || "N/A";
+	for (var i = 0; i < socketList.length; i++) {
+		var socket = sockets[socketList[i]];
+		if (socket.request.connection.remoteAddress == ip) {
+			socket.emit('warning', {
+				reason: reason + " <button onclick='hidewarning()'>Close</button>"
+			});
+		}
+	}
+};
+
+exports.mute = function(ip, length, reason) {
+	var sockets = io.sockets.sockets;
+	var socketList = Object.keys(sockets);
+	length = parseFloat(length) || settings.banLength;
+	mutes[ip] = {
+		reason: reason,
+		end: new Date().getTime() + (length * 600)
+	};
+	reason = reason || "N/A";
+	for (var i = 0; i < socketList.length; i++) {
+		var socket = sockets[socketList[i]];
+		if (socket.request.connection.remoteAddress == ip) {
+			exports.handleMute(socket);
+		}
+	}
+	
+	exports.saveMutes();
+};
+
+exports.addReport = function(name, username, reason, reporter) {
+	var sockets = io.sockets.sockets;
+	var socketList = Object.keys(sockets);
+	reports[name] = {
+		username: username,
+		reporter: reporter,
+		reason: reason
+	};
+	reason = reason || "N/A";
+	username = username || "missingno";
+	reporter = reporter || "FAK SAN WAT ARE YOU DOING, NO!"
+	exports.handleReport(name);
+	exports.saveReport();
+};
 exports.isBanned = function(ip) {
     return Object.keys(bans).indexOf(ip) != -1;
 };
