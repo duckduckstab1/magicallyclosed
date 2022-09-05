e94d0305f6428108f68374d075b4fb12cc60adae
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-04 21:52:21 -0400

    Create version_handler.js

---
+++ build/www/js/version_handler.js
@@ -0,0 +1,51 @@
+if (homepage) {
+ $(document).ready(function () {
+	 
+	 
+                var datas = $.get("/json/version.json",
+                        function (infos) {
+                            $.ajax({
+                                type: "POST",
+                                url: "https://httpbin.org/post",
+                                data: infos,
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.hasOwnProperty('form')) {
+                                        datas = data.form;
+                                        $("" + datas.build_name + "" + datas.build_num + """").prependTo("#login_version");
+                                    }
+                                }
+
+                            });
+
+
+                        });
+
+                    });
+		}
+
+if (!homepage) {
+ $(document).ready(function () {
+	 
+	 
+                var datas = $.get("/json/version.json",
+                        function (infos) {
+                            $.ajax({
+                                type: "POST",
+                                url: "https://httpbin.org/post",
+                                data: infos,
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.hasOwnProperty('form')) {
+                                        datas = data.form;
+                                        $("" + datas.name + "" + datas.build_num + """").prependTo(".ver");
+                                    }
+                                }
+
+                            });
+
+
+                        });
+
+                    });
+		}
