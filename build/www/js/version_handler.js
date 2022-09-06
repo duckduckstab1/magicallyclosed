9a19cbcec5915bd5364d7fefb55225aecb9bb8bf
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 12:59:11 -0400

    Update version_handler.js

--- build/www/js/version_handler.js
+++ build/www/js/version_handler.js
@@ -1,9 +1,4 @@
-// Fix for homepage detector
-var homepage = location.protocol + '//' + location.host;
-var homepage_2 = location.protocol + '//' + location.host + '/index.html';
-
-if (homepage) {
- $(document).ready(function () {
+$(document).ready(function () {
 	 
 	 
                 var datas = $.get("/json/version.json",
@@ -16,85 +11,7 @@ if (homepage) {
                                 success: function (data) {
                                     if (data.hasOwnProperty('form')) {
                                         datas = data.form;
-                                        $('' + datas.build_1 + '').appendTo("#login_version");
-                                    }
-                                }
-
-                            });
-
-
-                        });
-
-                    });
-		}
-
-if (homepage_2) {
- $(document).ready(function () {
-	 
-	 
-                var datas = $.get("/json/version.json",
-                        function (infos) {
-                            $.ajax({
-                                type: "POST",
-                                url: "https://httpbin.org/post",
-                                data: infos,
-                                dataType: "json",
-                                success: function (data) {
-                                    if (data.hasOwnProperty('form')) {
-                                        datas = data.form;
-                                        $('' + datas.build_1 + '').appendTo("#login_version");
-                                    }
-                                }
-
-                            });
-
-
-                        });
-
-                    });
-		}
-
-if (!homepage) {
- $(document).ready(function () {
-	 
-	 
-                var datas = $.get("/json/version.json",
-                        function (infos) {
-                            $.ajax({
-                                type: "POST",
-                                url: "https://httpbin.org/post",
-                                data: infos,
-                                dataType: "json",
-                                success: function (data) {
-                                    if (data.hasOwnProperty('form')) {
-                                        datas = data.form;
-                                        $(""+ datas.build_2 + "").appendTo(".ver");
-                                    }
-                                }
-
-                            });
-
-
-                        });
-
-                    });
-		}
-
-if (!homepage_2) {
- $(document).ready(function () {
-	 
-	 
-                var datas = $.get("/json/version.json",
-                        function (infos) {
-                            $.ajax({
-                                type: "POST",
-                                url: "https://httpbin.org/post",
-                                data: infos,
-                                dataType: "json",
-                                success: function (data) {
-                                    if (data.hasOwnProperty('form')) {
-                                        datas = data.form;
-                                        $("" + datas.build_2 + "").appendTo(".ver");
+                                        $("" + datas.build_1 + "").appendTo("#login_version");
                                     }
                                 }
 
