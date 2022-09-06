6c369014f3633ca683148a78ecc699b4b8cf2d9d
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 13:47:10 -0400

    Update version_handler_sub.js

--- build/www/js/version_handler_sub.js
+++ build/www/js/version_handler_sub.js
@@ -8,6 +8,15 @@
 
 $('.ver').html(data.handler.build); */
 
+function appendData(data) {
+  var mainContainer = $(".ver");
+  for (var i = 0; i < data.length; i++) {
+    var div = document.createElement("div");
+    div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
+    mainContainer.appendChild(div);
+  }
+}
+
 fetch('/json/version_readme.json')
   .then(function (response) {
     return response.json();
