5d318afe17d7c81796bdc393b845cc9f29c5f1c1
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 13:57:03 -0400

    Update pwa.js

--- build/www/js/lib/pwa.js
+++ build/www/js/lib/pwa.js
@@ -1,57 +1,19 @@
-// Stolen code https://web.dev/customize-install/
+let defferedPrompt;
+const addbtn = document.querySelector('.btn');
 
-
-// Initialize deferredPrompt for use later to show browser install prompt.
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-  // Optionally, send analytics event that PWA install promo was shown.
-  console.log(`'beforeinstallprompt' event was fired.`);
+window.addEventListener('beforeinstallprompt', event => {
+    event.preventDefault();
+    defferedPrompt = event
+    addbtn.style.display = 'block';
 });
 
-buttonInstall.addEventListener('click', async () => {
-  // Hide the app provided install promotion
-  hideInstallPromotion();
-  // Show the install prompt
-  deferredPrompt.prompt();
-  // Wait for the user to respond to the prompt
-  const { outcome } = await deferredPrompt.userChoice;
-  // Optionally, send analytics event with outcome of user choice
-  console.log(`User response to the install prompt: ${outcome}`);
-  // We've used the prompt, and can't use it again, throw it away
-  deferredPrompt = null;
-});
+addbtn.addEventListener('click', event => {
+    defferedPrompt.prompt();
 
-window.addEventListener('appinstalled', () => {
-  // Hide the app-provided install promotion
-  hideInstallPromotion();
-  // Clear the deferredPrompt so it can be garbage collected
-  deferredPrompt = null;
-  // Optionally, send analytics event to indicate successful install
-  console.log('PWA was installed');
-});
-
-function getPWADisplayMode() {
-  const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-  if (document.referrer.startsWith('android-app://')) {
-    return 'twa';
-  } else if (navigator.standalone || isStandalone) {
-    return 'standalone';
-  }
-  return 'browser';
-}
-
-window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
-  let displayMode = 'browser';
-  if (evt.matches) {
-    displayMode = 'standalone';
-  }
-  // Log display mode change to analytics
-  console.log('DISPLAY_MODE_CHANGED', displayMode);
-});
+    defferedPrompt.userChoice.then(choice => {
+        if(choice.outcome === 'accepted'){
+            console.log('user accepted the prompt')
+        }
+        defferedPrompt = null;
+    })
+})
