ab63731b6cf923a4b9568f6c7cb0edd6e981f1d7
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 09:20:20 -0400

    Update version_handler.js

--- build/www/js/version_handler.js
+++ build/www/js/version_handler.js
@@ -1,4 +1,34 @@
+// Fix for homepage detector
+var homepage = location.protocol + '//' + location.host;
+var homepage_2 = location.protocol + '//' + location.host + '/index.html';
+
 if (homepage) {
+ $(document).ready(function () {
+	 
+	 
+                var datas = $.get("/json/version.json",
+                        function (infos) {
+                            $.ajax({
+                                type: "POST",
+                                url: "https://httpbin.org/post",
+                                data: infos,
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.hasOwnProperty('form')) {
+                                        datas = data.form;
+                                        $("" + datas.build_name + "" + datas.build_num + """").appendTo("#login_version");
+                                    }
+                                }
+
+                            });
+
+
+                        });
+
+                    });
+		}
+
+if (homepage_2) {
  $(document).ready(function () {
 	 
 	 
@@ -38,7 +68,33 @@ if (!homepage) {
                                 success: function (data) {
                                     if (data.hasOwnProperty('form')) {
                                         datas = data.form;
-                                        $("" + datas.name + "" + datas.build_num + """").appendTo(".ver");
+                                        $("" + datas.name + "" + datas.build_num + "").appendTo(".ver");
+                                    }
+                                }
+
+                            });
+
+
+                        });
+
+                    });
+		}
+
+if (!homepage) {
+ $(document).ready(function () {
+	 
+	 
+                var datas = $.get("/json/version.json",
+                        function (infos) {
+                            $.ajax({
+                                type: "POST",
+                                url: "https://httpbin.org/post",
+                                data: infos,
+                                dataType: "json",
+                                success: function (data) {
+                                    if (data.hasOwnProperty('form')) {
+                                        datas = data.form;
+                                        $("" + datas.name + "" + datas.build_num + "").appendTo(".ver");
                                     }
                                 }
 
