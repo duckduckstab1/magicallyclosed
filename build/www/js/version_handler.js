250ecda72a260899a3e9fc2055904e2b89529775
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 13:42:20 -0400

    Update version_handler.js

--- build/www/js/version_handler.js
+++ build/www/js/version_handler.js
@@ -1,8 +1,20 @@
-var data = {
+// too simple
+/* var data = {
   "handler": {
     "build": "1.7.2",
   }
 }
 
 
-$('#login_version').html(data.handler.build);
+$('#login_version').html(data.handler.build); */
+
+fetch('/json/version_homepage.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
