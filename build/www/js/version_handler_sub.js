ea272c1f0f531577536a8f9414b1be806bbac1fb
Cosmic <58494315+CosmicStar98@users.noreply.github.com>
2022-09-06 13:42:56 -0400

    Update version_handler_sub.js

--- build/www/js/version_handler_sub.js
+++ build/www/js/version_handler_sub.js
@@ -1,8 +1,20 @@
-var data = {
+// too simple
+/* var data = {
   "handler": {
     "build": "BonziWORLD Revived+ - 1.7.2",
   }
 }
 
 
-$('.ver').html(data.handler.build);
+$('.ver').html(data.handler.build); */
+
+fetch('/json/version_readme.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
